ea4869a91e9098faea0b8e087584935640b52bd6
Shank <sankarams18@gmail.com>
2024-10-15 01:04:16 -0400

    Updates

---
+++ precache-manifest.fbe71dcdf72030a8841d329a4a6a7839.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef896681c5ebb9d68e40ddb47d80f93",
+    "url": "/myPortfolio.git/index.html"
+  },
+  {
+    "revision": "85d2ae510a717e0fd8eb",
+    "url": "/myPortfolio.git/static/css/main.2ed3a574.chunk.css"
+  },
+  {
+    "revision": "801db6b8573e5e5c0bdc",
+    "url": "/myPortfolio.git/static/js/2.3df64f54.chunk.js"
+  },
+  {
+    "revision": "85d2ae510a717e0fd8eb",
+    "url": "/myPortfolio.git/static/js/main.6c8818ed.chunk.js"
+  },
+  {
+    "revision": "338d4bc95824d78850f7",
+    "url": "/myPortfolio.git/static/js/runtime~main.cd51a686.js"
+  },
+  {
+    "revision": "9b84dad7ee4cad349956ae0a808ddd80",
+    "url": "/myPortfolio.git/static/media/Apple.9b84dad7.png"
+  },
+  {
+    "revision": "f6493e6f36c10f9f1650dfae5d8099b6",
+    "url": "/myPortfolio.git/static/media/HTS.f6493e6f.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/myPortfolio.git/static/media/JS.0cded3a3.png"
+  },
+  {
+    "revision": "834c5feba01b6904b44cf6864a5eb883",
+    "url": "/myPortfolio.git/static/media/Moodys-Logo.834c5feb.png"
+  },
+  {
+    "revision": "a07108db17b266b6a89f1861b4817952",
+    "url": "/myPortfolio.git/static/media/PSG.a07108db.png"
+  },
+  {
+    "revision": "1a137d7af8bf6725fe82f33a2a3158c6",
+    "url": "/myPortfolio.git/static/media/Py.1a137d7a.png"
+  },
+  {
+    "revision": "d245ad9e627125fa1debe131960f7a83",
+    "url": "/myPortfolio.git/static/media/SQL.d245ad9e.png"
+  },
+  {
+    "revision": "87b8fcc49f4d4bf71192ac0fab50cc14",
+    "url": "/myPortfolio.git/static/media/buffalo.87b8fcc4.jpg"
+  },
+  {
+    "revision": "f6598e5cd938c3e6d891ac680dcb1eb0",
+    "url": "/myPortfolio.git/static/media/contact.f6598e5c.png"
+  },
+  {
+    "revision": "f47ca27e479552d5f399577f75e458e8",
+    "url": "/myPortfolio.git/static/media/discord.f47ca27e.png"
+  },
+  {
+    "revision": "9d07141dd9a4b776147d16a109d78dff",
+    "url": "/myPortfolio.git/static/media/facebook.9d07141d.png"
+  },
+  {
+    "revision": "ca6c1ec912dc6ba27e886b09b79578bd",
+    "url": "/myPortfolio.git/static/media/hireMe.ca6c1ec9.png"
+  },
+  {
+    "revision": "ba1e4a0195d02fd54d1ef7c06cb51e3a",
+    "url": "/myPortfolio.git/static/media/java.ba1e4a01.png"
+  },
+  {
+    "revision": "932b151e86b9d4cdef9bc13fa3725374",
+    "url": "/myPortfolio.git/static/media/javapng.932b151e.jpg"
+  },
+  {
+    "revision": "b2bd15cbc5bcf8ce0a075ec61fc6f62e",
+    "url": "/myPortfolio.git/static/media/logo.b2bd15cb.png"
+  },
+  {
+    "revision": "469cf45846588a42ae4da639597b78f3",
+    "url": "/myPortfolio.git/static/media/profile.469cf458.png"
+  },
+  {
+    "revision": "232cc49284a9c8b65fa2fb36451e6563",
+    "url": "/myPortfolio.git/static/media/react.232cc492.png"
+  },
+  {
+    "revision": "8c60dcfd0cfc777a1cd532d953fee49a",
+    "url": "/myPortfolio.git/static/media/translation.8c60dcfd.png"
+  },
+  {
+    "revision": "f8d6600d93011e758e2000737a93858f",
+    "url": "/myPortfolio.git/static/media/weather.f8d6600d.png"
+  }
+]);
